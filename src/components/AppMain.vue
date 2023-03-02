d8e6cbe65cb3295fab2c9cfca5a0caf88c0c5992
CastellucciLuca <castellucciluca98@gmail.com>
2023-03-02 17:13:46 +0100

    Add type and technology

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,9 +5,18 @@
                 <h2>Posts:</h2>
             </div>
             <article class="col-6 single-post card mb-3 p-3" v-for="post in posts">
+                    <h6 class="card-header">
+                        {{ post.type.name }} --- {{ post.author }}
+                    </h6>
                     <img :src="post.image" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">{{ post.title }}</h5>
+                        <p>
+                            <span class="little-technlogy me-3" v-for="technology in post.technologies">
+                                #{{ technology.name }}
+                            </span>
+                        </p>
+                        <h6 class="card-subtitle">{{ post.post_date }}</h6>
                         <p class="card-text">{{ post.content.substr(0,250)}}...</p>
                         <a href="#" class="btn btn-primary">Read More...</a>
                     </div>
